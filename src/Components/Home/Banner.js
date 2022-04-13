11e48364e9bb063c324a7e772fc781e5e4fa5fcb
shakil89427 <shakilahmed89427@gmail.com>
2022-04-14 01:34:26 +0600

    banner updated

--- src/Components/Home/Banner.js
+++ src/Components/Home/Banner.js
@@ -1,17 +1,13 @@
 import React from "react";
 import NavBar from "../NavBar/NavBar";
-import "./Banner.css";
+import "./CSS/Banner.css";
 import { BsSearch } from "react-icons/bs";
-import bannerBg1 from "../../Assets/bannerBg1.png";
-import bannerBg2 from "../../Assets/bannerBg2.png";
-import bannerBg3 from "../../Assets/bannerBg3.png";
 
 /* Styles Start */
 const styles = {
   main: "flex items-center text-white lg:h-[75vh] relative px-6 md:px-20 lg:px-24",
-  bgColor: "absolute inset-0 -z-10",
   bgImage:
-    "absolute inset-0 mr-10 bg-no-repeat bg-contain bg-right-bottom hidden lg:block -z-10 ",
+    "absolute inset-0 mr-10 bg-no-repeat bg-contain bg-right-bottom hidden lg:block z-10 bgImage",
   heading: "text-5xl lg:text-6xl font-semibold",
   formMain:
     "flex flex-col md:flex-row gap-2 md:gap-0 md:rounded-md overflow-hidden mt-8 mb-5 w-full lg:w-5/6",
@@ -28,39 +24,11 @@ const styles = {
 
 const Banner = () => {
   return (
-    <div className="relative">
-      {/* Bg Color Start */}
-      <div
-        style={{ backgroundImage: "radial-gradient(#FF9826, #E77B02)" }}
-        className={`${styles.bgColor} bgColor`}
-      />
-      <div
-        style={{ backgroundImage: "radial-gradient(circle,#C41BFF, #8C03BC)" }}
-        className={`${styles.bgColor} bgColor2`}
-      />
-      <div
-        style={{ backgroundImage: "radial-gradient(circle,#FF1B6D, #CD004A)" }}
-        className={`${styles.bgColor} bgColor3`}
-      />
+    <div className="bgColor">
       <NavBar color={"transparent"} />
-      {/* Bg Color End */}
       <div className={styles.main}>
-        {/* Bg Image Start */}
-        <div
-          style={{ backgroundImage: `url(${bannerBg1})` }}
-          className={`${styles.bgImage} bgImage1`}
-        />
-        <div
-          style={{ backgroundImage: `url(${bannerBg2})` }}
-          className={`${styles.bgImage} bgImage2`}
-        />
-        <div
-          style={{ backgroundImage: `url(${bannerBg3})` }}
-          className={`${styles.bgImage} bgImage3`}
-        />
-        {/* Bg Image End */}
-
-        <div className="lg:w-4/6 my-5">
+        <div className={styles.bgImage} />
+        <div className="lg:w-4/6 z-20 my-5">
           <h1 style={{ lineHeight: "115%" }} className={styles.heading}>
             Find <span className="font-bold">Influencers & Brands</span> for
             collaboration
