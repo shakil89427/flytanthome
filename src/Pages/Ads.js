bef8a0673e7689e70cf46135a70c929b66a0803f
shakil89427 <shakilahmed89427@gmail.com>
2022-04-19 23:48:36 +0600

    privacy done

--- src/Pages/Ads.js
+++ src/Pages/Ads.js
@@ -10,14 +10,14 @@ const Ads = () => {
         We want to show you ads that are meaningful and you find them
         interesting and useful. Here’s an overview of how Flytant Ads work.
       </h4>
-      <div className="my-32">
-        <h4 className="text-xl md:text-2xl lg:text-3xl font-medium  text-gray-800">
-          What are Flytant Ads?
-        </h4>
-        <li
-          style={{ lineHeight: "200%" }}
-          className="ml-10 mt-14 text-md md:text-xl"
-        >
+      <h4 className="text-xl md:text-2xl lg:text-3xl font-medium  text-gray-800 mt-32 mb-14">
+        What are Flytant Ads?
+      </h4>
+      <div
+        style={{ lineHeight: "200%" }}
+        className="ml-10 mt-14 text-md md:text-xl"
+      >
+        <li>
           You may see various kinds of ads on Flytant, including Sponsored Ads,
           Promoted Ads and Promoted Posts. We can show these ads to you when you
           are logged in or logged out of Flytant. They are clearly marked with a
@@ -25,22 +25,19 @@ const Ads = () => {
           the same way as organic content.
         </li>
       </div>
-      <div className="my-32">
-        <h4 className="text-xl md:text-2xl lg:text-3xl font-medium  text-gray-800">
-          Why you see certain Ads?
-        </h4>
-        <li
-          style={{ lineHeight: "200%" }}
-          className="ml-10 mt-14 text-md md:text-xl"
-        >
+      <h4 className="text-xl md:text-2xl lg:text-3xl font-medium  text-gray-800 mt-32 mb-14">
+        Why you see certain Ads?
+      </h4>
+      <div
+        style={{ lineHeight: "200%" }}
+        className="ml-10 mt-14 text-md md:text-xl flex flex-col gap-10"
+      >
+        <li>
           Your activity on Flytant, the information you provide to Flytant, and
           our relationships with ad partners all help to make the sponsored
           content more relevant for you.
         </li>
-        <li
-          style={{ lineHeight: "200%" }}
-          className="ml-10 mt-14 text-md md:text-xl"
-        >
+        <li>
           When you use Flytant to follow, Like, search, view, or interact with
           posts or Flytant accounts, we may use these actions to customise
           Flytant Ads for you. Have more fun with For example, if you search for
@@ -51,19 +48,13 @@ const Ads = () => {
           installed on your device. This helps us show you local ads and other
           ads that you might prefer.
         </li>
-        <li
-          style={{ lineHeight: "200%" }}
-          className="ml-10 mt-14 text-md md:text-xl"
-        >
+        <li>
           Flytant may also personalise ads based on information that Flytant and
           our affiliates collect and that our ad partners share with us, such as
           a hashed email address, a mobile device identifier, or browser-related
           information (a browser cookie ID).
         </li>
-        <li
-          style={{ lineHeight: "200%" }}
-          className="ml-10 mt-14 text-md md:text-xl"
-        >
+        <li>
           This helps Flytant display ads about things you’ve already shown
           interest in from brands and businesses that you may like.. You could
           also see this business as a Promoted Account in one of your “Who to
