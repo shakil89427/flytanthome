8c0d7045d34d6ec2d3ad9e6c0824c0d8b5829db6
shakil89427 <shakilahmed89427@gmail.com>
2022-04-19 21:28:55 +0600

    onboard size change

--- src/Components/Home/PublicContent/OnboardHero.js
+++ src/Components/Home/PublicContent/OnboardHero.js
@@ -8,7 +8,7 @@ const OnboardHero = () => {
         backgroundImage: "linear-gradient(to right,rgb(78, 77, 77), black)",
       }}
     >
-      <div className="flex flex-col-reverse md:flex-row  max-w-[1200px] mx-auto py-20">
+      <div className="flex flex-col-reverse md:flex-row md:mx-10 py-20">
         <div className="relative w-full md:w-7/12 p-10 bg-black py-10 before:content-[''] before:absolute before:h-full before:w-1/2 before:bg-white before:top-0 before:left-0">
           <img className="relative rounded-3xl z-40" src={onboardHero} alt="" />
         </div>
@@ -26,7 +26,7 @@ const OnboardHero = () => {
             >
               all in one spot
             </h1>
-            <p className="text-xl font-medium my-8">
+            <p className="text-xl font-medium max-w-[450px] my-8">
               Flytant is the go to platform for Influencer Marketing. We provide
               a holistic platform for Influencers and Brands to connect together
               transparently and make the most of their Collaboration.
