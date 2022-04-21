b0b587716a69aaca71078f3495b1222d64c8a3df
shakil89427 <shakilahmed89427@gmail.com>
2022-04-21 15:03:09 +0600

    nav button changed

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -15,7 +15,7 @@ const styles = {
   logo: "cursor-pointer w-[130px] md:w-[200px]",
   profile: "flex items-center text-white",
   profileImg: "w-9 h-9 md:w-10 md:h-10 rounded-full mr-2",
-  btn: "bg-gray-600 text-xs md:text-sm  z-20 relative border-0 px-3 md:px-6 py-2 md:py-3 md:text-md text-white duration-300 before:border-r-2 before:duration-300 before:content-[''] before:absolute before:h-full  before:w-0 before:top-0 before:left-0 before:bg-gray-800 before:-z-10 hover:before:w-full overflow-hidden",
+  btn: "text-xs border border-gray-400 text-gray-300 px-4 rounded-lg md:px-6 py-2 md:py-3 md:text-md text-white duration-150 hover:scale-105 hover:border-white hover:text-white",
 };
 /* Styles End */
 
@@ -51,7 +51,7 @@ const NavBar = ({ color }) => {
           />
         </div>
       ) : (
-        <div className="flex items-center gap-4">
+        <div className="flex items-center gap-3">
           <a
             href="mailto:contact@flytant.com"
             target="_blank"
