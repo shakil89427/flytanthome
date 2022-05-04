68c500778020ee89d401dedd478ffab2ebf84271
shakil89427 <shakilahmed89427@gmail.com>
2022-05-05 01:27:28 +0600

    video id loaded

--- src/Hooks/useYoutubeConnect.js
+++ src/Hooks/useYoutubeConnect.js
@@ -5,7 +5,48 @@ import { useLocation } from "react-router-dom";
 const useYoutubeConnect = () => {
   const location = useLocation();
 
-  const getplayLists = async (token) => {
+  const getPlaylistsItems = async (playlistId) => {
+    try {
+      const response = await axios.get(
+        "https://www.googleapis.com/youtube/v3/playlistItems",
+        {
+          params: {
+            part: "contentDetails",
+            playlistId,
+            key: process.env.REACT_APP_GOOGLE_API_KEY,
+          },
+        }
+      );
+      const videoIds = response?.data?.items?.map(
+        (item) => item.contentDetails
+      );
+      console.log(videoIds);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const getPlaylists = async (channelId) => {
+    try {
+      const response = await axios.get(
+        "https://www.googleapis.com/youtube/v3/playlists",
+        {
+          params: {
+            part: "id",
+            channelId,
+            key: process.env.REACT_APP_GOOGLE_API_KEY,
+          },
+        }
+      );
+      const playlists = response?.data?.items?.map((item) => item.id);
+      console.log("Playlists", playlists);
+      getPlaylistsItems(playlists[0]);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const getChannels = async (token) => {
     try {
       const response = await axios.get(
         "https://www.googleapis.com/youtube/v3/channels",
@@ -19,6 +60,7 @@ const useYoutubeConnect = () => {
       );
       const channels = response?.data?.items?.map((item) => item.id);
       console.log("Channels", channels);
+      getPlaylists(channels[0]);
     } catch (err) {
       console.log(err);
     }
@@ -27,7 +69,7 @@ const useYoutubeConnect = () => {
   useEffect(() => {
     if (location?.hash?.includes("state=youtubev3")) {
       const token = location?.hash?.split("token=")[1]?.split("&token_type")[0];
-      getplayLists(token);
+      getChannels(token);
     }
   }, []);
 };
