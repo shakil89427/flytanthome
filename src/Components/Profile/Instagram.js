874c8f2f13dee26a74ed5585ecbf86798e3864d3
shakil89427 <shakilahmed89427@gmail.com>
2022-05-07 21:48:40 +0600

    instagram connected

--- src/Components/Profile/Instagram.js
+++ src/Components/Profile/Instagram.js
@@ -1,18 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import instagram from "../../Assets/profileSocials/instagram.png";
+import Spinner from "../../Components/Spinner/Spinner";
 import useConnect from "../../Hooks/Instagram/useConnect";
 
 const Instagram = ({ details }) => {
-  const { openPopup } = useConnect();
-  return (
-    <div className="flex flex-col items-center gap-5 mt-40 text-gray-500 text-sm font-medium">
-      <p>No account linked</p>
+  const [loading, setLoading] = useState(false);
+  const { openPopup } = useConnect(setLoading);
 
-      <p onClick={openPopup}>
-        <img className="w-1/2 mx-auto" src={instagram} alt="" />
-      </p>
-      <p>Click here to link your instagram</p>
-    </div>
+  useEffect(() => {}, [details]);
+  return (
+    <>
+      {loading && <Spinner />}
+      {!loading && !details?.linkedAccounts?.Instagram && details?.access && (
+        <div className="flex flex-col items-center gap-5 mt-40 text-gray-500 text-sm font-medium">
+          <p>No account linked</p>
+
+          <p onClick={openPopup}>
+            <img className="w-1/2 mx-auto" src={instagram} alt="" />
+          </p>
+          <p>Click here to link your instagram</p>
+        </div>
+      )}
+    </>
   );
 };
 
--- src/Hooks/Instagram/useConnect.js
+++ src/Hooks/Instagram/useConnect.js
@@ -1,48 +1,74 @@
 import axios from "axios";
+import { doc, getFirestore, updateDoc } from "firebase/firestore";
 import useStore from "../../Store/useStore";
 
-const useConnect = () => {
-  const { setNotify } = useStore();
-  const instaClientId = process.env.REACT_APP_INSTAGRAM_CLIENT_ID;
-  const instaClientSecret = process.env.REACT_APP_INSTAGRAM_CLIENT_SECRET;
-  const instaRedirectUri = process.env.REACT_APP_INSTAGRAM_REDIRECT_URI;
+const useConnect = (setLoading) => {
+  const db = getFirestore();
+  const { setNotify, user, setUser } = useStore();
 
-  const getFullData = async (username) => {
-    try {
-      const response = await axios.get(
-        `https://www.instagram.com/${username}/channel/?__a=1`
-      );
-      console.log(response);
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  const getuserInfo = async (access_token) => {
-    try {
-      const response = await axios.get(
-        `https://graph.instagram.com/me?fields=id,username&access_token=${access_token}`
-      );
-      if (response?.data) {
-        getFullData(response.data.username);
-      }
-    } catch (err) {
-      setNotify({ status: false, message: err?.message });
-    }
+  /* Add token to user db */
+  const addTokenToUser = (data) => {
+    const tokenInfo = {
+      accessToken: data?.access_token,
+      tokenExpires: Date.now() + data?.expires_in * 1000,
+    };
+    axios
+      .get(
+        `https://graph.instagram.com/me?fields=id,username&access_token=${tokenInfo?.accessToken}`
+      )
+      .then((res) => {
+        const info = { username: res.data.username, instaId: res.data.id };
+        const updatedData = {
+          linkedAccounts: user?.linkedAccounts
+            ? { ...user?.linkedAccounts, Instagram: { ...info, ...tokenInfo } }
+            : { Instagram: { ...info, ...tokenInfo } },
+        };
+        const userRef = doc(db, "users", user?.id);
+        updateDoc(userRef, updatedData)
+          .then(() => {
+            setUser({ ...user, ...updatedData });
+            setLoading(false);
+            setNotify({
+              status: true,
+              message: "Instagram linked successfully",
+            });
+          })
+          .catch((err) => {
+            setLoading(false);
+            setNotify({ status: false, message: "Cannot link Instagram" });
+          });
+      })
+      .catch((err) => {
+        setLoading(false);
+        setNotify({ status: false, message: "Cannot link Instagram" });
+      });
   };
 
   /* Get Token */
   const getToken = async (code) => {
-    const url = "https://api.instagram.com/oauth/access_token";
-    const headers = {
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-    };
-    const params = `client_id=${instaClientId}&client_secret=${instaClientSecret}&grant_type=authorization_code&redirect_uri=${instaRedirectUri}&code=${code}`;
+    setLoading(true);
     try {
-      const response = await axios.post(url, params, headers);
-      getuserInfo(response.data.access_token);
+      /* Short live token */
+      const response1 = await axios.post(
+        "https://api.instagram.com/oauth/access_token",
+        `client_id=${process.env.REACT_APP_INSTAGRAM_CLIENT_ID}&client_secret=${process.env.REACT_APP_INSTAGRAM_CLIENT_SECRET}&grant_type=authorization_code&redirect_uri=${process.env.REACT_APP_INSTAGRAM_REDIRECT_URI}&code=${code}`,
+        { headers: { "Content-Type": "application/x-www-form-urlencoded" } }
+      );
+      /* Long live token */
+      const response2 = await axios.get(
+        "https://graph.instagram.com/access_token",
+        {
+          params: {
+            grant_type: "ig_exchange_token",
+            client_secret: process.env.REACT_APP_INSTAGRAM_CLIENT_SECRET,
+            access_token: response1.data.access_token,
+          },
+        }
+      );
+      addTokenToUser(response2.data);
     } catch (err) {
-      setNotify({ status: false, message: err?.message });
+      setLoading(false);
+      setNotify({ status: false, message: "Cannot link Instagram" });
     }
   };
 
@@ -70,3 +96,14 @@ const useConnect = () => {
 };
 
 export default useConnect;
+
+// const getFullData = async (username) => {
+//   try {
+//     const response = await axios.get(
+//       `https://www.instagram.com/${username}/channel/?__a=1`
+//     );
+//     console.log(response);
+//   } catch (err) {
+//     console.log(err);
+//   }
+// };
