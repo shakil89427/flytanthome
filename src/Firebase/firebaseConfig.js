927e5fbe66cf972901f96c112fff3fe706b5d427
shakil89427 <shakilahmed89427@gmail.com>
2022-05-19 15:59:24 +0600

    new db added

--- src/Firebase/firebaseConfig.js
+++ src/Firebase/firebaseConfig.js
@@ -5,6 +5,7 @@ const firebaseConfig = {
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
   appId: process.env.REACT_APP_FIREBASE_APPID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENTID,
 };
 
 export default firebaseConfig;
