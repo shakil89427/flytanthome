ec6b74d4b1f7bf9eab5a9bcb73bec3ee4d11d798
shakil89427 <shakilahmed89427@gmail.com>
2022-05-21 00:12:18 +0600

    checked

--- src/Hooks/useCheckCode.js
+++ src/Hooks/useCheckCode.js
@@ -1,16 +1,19 @@
+import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
 const useCheckCode = () => {
   const location = useLocation();
 
-  if (location?.search?.includes("state=access")) {
-    localStorage.setItem("access", location?.search);
-    window.close();
-  }
-  if (location?.hash?.includes("state=access")) {
-    localStorage.setItem("access", location?.hash);
-    window.close();
-  }
+  useEffect(() => {
+    if (location?.search?.includes("state=access")) {
+      localStorage.setItem("access", location?.search);
+      window.close();
+    }
+    if (location?.hash?.includes("state=access")) {
+      localStorage.setItem("access", location?.hash);
+      window.close();
+    }
+  }, []);
 };
 
 export default useCheckCode;
