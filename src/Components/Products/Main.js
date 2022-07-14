8a00414ed4b44a56efb93aa4a881ad90f2d4f7bd
shakil89427 <shakilahmed89427@gmail.com>
2022-07-14 18:53:32 +0600

    product details created

--- src/Components/Products/Main.js
+++ src/Components/Products/Main.js
@@ -8,72 +8,74 @@ const Main = () => {
   const { products } = useStore();
   const navigate = useNavigate();
   return (
-    <div className="max-w-[1200px] mx-auto">
-      <p className="font-semibold text-lg md:text-xl xl:text-3xl">
+    <div className="grid grid-cols-1 lg:grid-cols-2 gap-y-10 lg:gap-10 w-fit mx-auto">
+      <p className="font-semibold text-lg md:text-xl xl:text-3xl lg:col-span-2">
         Social Cards
       </p>
-      <div className="mt-10 grid grid-cols-1 lg:grid-cols-2 gap-y-10 lg:gap-10 w-fit mx-auto">
-        {products?.map((product) => (
-          <div
-            key={product?.id}
-            style={{
-              boxShadow: `0px 0px 15px 1px rgba(13,12,12,.10)`,
-            }}
-            onClick={() => navigate(`/products/${product?.id}`)}
-            className="bg-[#F8F8F8] grid grid-cols-1 lg:grid-cols-2 rounded-xl overflow-hidden cursor-pointer max-w-[350px] lg:max-w-[500px]"
-          >
-            <div className="bg-white rounded-xl p-5 min-h-[250px] flex items-center justify-center">
-              <img src={product.blob[0].url} alt="" />
-            </div>
-            <div className="p-5 flex flex-col items-center lg:items-start">
-              <p className="font-bold text-lg lg:text-xl xl:text-2xl">
-                {product?.name}
-              </p>
-              {product?.customizable && (
-                <p className="text-sm mt-1">Customizable</p>
-              )}
-
-              {product?.quantityLeft > 100 ? (
-                <p className="text-xs w-fit lg:mx-0 px-5 py-1 font-medium rounded-full bg-[#27C200] text-white mt-3">
-                  Special Price
-                </p>
-              ) : (
-                <p className="text-xs w-fit lg:mx-0 px-5 py-1 font-medium rounded-full bg-[#FF9345] text-white mt-3">
-                  Only {product?.quantityLeft} left
-                </p>
-              )}
-
-              <div className="flex items-center gap-x-1 mt-3">
-                <Rating
-                  className="text-md text-[#F7C02B] pt-1"
-                  initialRating={product?.rating?.ratingCount}
-                  readonly
-                  emptySymbol={<AiOutlineStar />}
-                  fullSymbol={<AiFillStar />}
-                />
-                <p className="text-sm font-medium pl-1 text-gray-600">
-                  {product?.rating?.ratingCount} Rating
-                </p>
-              </div>
-              <div className="flex items-center gap-5 mt-3">
-                <p className="font-bold text-lg md:text-xl lg:text-2xl">
-                  {product?.priceData?.symbol}
-                  {product?.priceData?.priceNow}
-                </p>
-                <p className="text-[#06A015] font-semibold">
-                  {product?.priceData?.discount}% off
-                </p>
-              </div>
-              <p className="text-sm md:text-md lg:text-lg font-medium">
-                <del>
-                  {product?.priceData?.symbol}
-                  {product?.priceData?.pricePrev}
-                </del>
-              </p>
-            </div>
+      {products?.map((product) => (
+        <div
+          key={product?.id}
+          style={{
+            boxShadow: `0px 0px 15px 1px rgba(13,12,12,.10)`,
+          }}
+          onClick={() => navigate(`/products/${product?.id}`)}
+          className="bg-[#F8F8F8] grid grid-cols-1 lg:grid-cols-2 rounded-xl overflow-hidden cursor-pointer max-w-[350px] lg:max-w-[500px]"
+        >
+          <div className="bg-white flex items-center justify-center rounded-xl">
+            <div
+              style={{ backgroundImage: `url(${product.blob[0].url})` }}
+              className="w-full aspect-square bg-contain bg-center bg-no-repeat"
+            />
           </div>
-        ))}
-      </div>
+
+          <div className="p-5 flex flex-col items-center lg:items-start">
+            <p className="font-bold text-lg lg:text-xl xl:text-2xl">
+              {product?.name}
+            </p>
+            {product?.customizable && (
+              <p className="text-sm mt-1">Customizable</p>
+            )}
+
+            {product?.quantityLeft > 100 ? (
+              <p className="text-xs w-fit lg:mx-0 px-5 py-1 font-medium rounded-full bg-[#27C200] text-white mt-3">
+                Special Price
+              </p>
+            ) : (
+              <p className="text-xs w-fit lg:mx-0 px-5 py-1 font-medium rounded-full bg-[#FF9345] text-white mt-3">
+                Only {product?.quantityLeft} left
+              </p>
+            )}
+
+            <div className="flex items-center gap-x-1 mt-3">
+              <Rating
+                className="text-md text-[#F7C02B] pt-1"
+                initialRating={product?.rating?.ratingCount}
+                readonly
+                emptySymbol={<AiOutlineStar />}
+                fullSymbol={<AiFillStar />}
+              />
+              <p className="text-sm font-medium pl-1 text-gray-600">
+                {product?.rating?.ratingCount} Rating
+              </p>
+            </div>
+            <div className="flex items-center gap-5 mt-3">
+              <p className="font-bold text-lg md:text-xl lg:text-2xl">
+                {product?.priceData?.symbol}
+                {product?.priceData?.priceNow}
+              </p>
+              <p className="text-[#06A015] font-semibold">
+                {product?.priceData?.discount}% off
+              </p>
+            </div>
+            <p className="text-sm md:text-md lg:text-lg font-semibold text-gray-500">
+              <del>
+                {product?.priceData?.symbol}
+                {product?.priceData?.pricePrev}
+              </del>
+            </p>
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
