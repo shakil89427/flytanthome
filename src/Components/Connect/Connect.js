6e35325e206db1d141b8c6d8d94f9359f63b6f6d
shakil89427 <shakilahmed89427@gmail.com>
2022-07-21 19:52:10 +0600

    checked

--- src/Components/Connect/Connect.js
+++ src/Components/Connect/Connect.js
@@ -47,12 +47,12 @@ const Connect = ({ cardUser, followData, setFollowData, setShowConnect }) => {
       const cardUserRef = doc(
         db,
         "users",
-        cardUser?.id,
+        cardUser.id,
         "followers",
-        user?.userId
+        user.userId
       );
-      const userRef = doc(db, "users", user?.userId, "following", cardUser?.id);
-      if (followData?.userId === user?.userId) {
+      const userRef = doc(db, "users", user.userId, "following", cardUser.id);
+      if (followData?.userId) {
         setFollowData({});
         deleteDoc(cardUserRef);
         deleteDoc(userRef);
@@ -71,7 +71,7 @@ const Connect = ({ cardUser, followData, setFollowData, setShowConnect }) => {
             user?.profileImageUrl ||
             "https://firebasestorage.googleapis.com/v0/b/flytant-cb72e.appspot.com/o/default_user_image%2FGroup%201763.png?alt=media&token=ef261d0b-a8d9-4791-a0c5-2d8338b77ea3",
           userId: user?.userId,
-          username: user?.username,
+          username: user?.username || "",
         };
         setFollowData(userData);
         setDoc(cardUserRef, userData);
